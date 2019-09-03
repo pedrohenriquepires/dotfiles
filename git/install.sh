7244df634bf8614c5869e9bd9b934030a9649590
Pedro Henrique Pires <pdrhenriquepires@gmail.com>
2019-09-03 15:07:15 -0300

    updating git config

--- git/install.sh
+++ git/install.sh
@@ -2,12 +2,17 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-source=$DIR/.gitconfig
-target=$HOME/.gitconfig
+config_source=$DIR/.gitconfig
+config_target=$HOME/.gitconfig
+gitignore_source=$DIR/.gitignore_global
+gitignore_target=$HOME/.gitignore_global
 
 if [ ! -L "$target" ]; then
-  echo "Copying $source to $target"
-  yes | cp -rf $source $target
+  echo "Copying $config_source to $config_target"
+  yes | cp -rf $config_source config_$target
+
+  echo "Copying $gitignore_source to $gitignore_target"
+  yes | cp -rf $gitignore_source $gitignore_target
 fi
 
 echo "Git setup successful."
